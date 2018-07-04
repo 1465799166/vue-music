d730882768bb030ec921af493e777df46dc2ea0f
sjh <1465799166@qq.com>
2018-07-04 15:05:34 +0800

    start sliter

--- src/App.vue
+++ src/App.vue
@@ -7,18 +7,17 @@
 </template>
 
 <script type="text/ecmascript-6">
-  import MHeader from 'components/m-header/m-header'
-  import Tab from 'components/tab/tab'
-  export default {
-    components: {
-      MHeader,
-      Tab
-    }
+import MHeader from 'components/m-header/m-header'
+import Tab from 'components/tab/tab'
+export default {
+  components: {
+    MHeader,
+    Tab
   }
+}
 </script>
 
 <style lang="stylus">
-
 
 #app {
 
---
+++ src/api/config.js
@@ -0,0 +1,14 @@
+// 通用参数
+export const commonParams = {
+  g_tk: 5381,
+  inCharset: 'utf-8',
+  outCharset: 'utf-8',
+  notice: 0,
+  format: 'jsonp'
+}
+
+export const options = {
+  param: 'jsonpCallback'
+}
+// 返回数据状态码
+export const ERR_OK = 0

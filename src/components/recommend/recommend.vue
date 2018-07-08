995fbd4f25abec24fdadd9fa254741cdc061d73e
sjh <1465799166@qq.com>
2018-07-08 20:03:13 +0800

    recommend v3

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,22 +1,32 @@
 <template>
     <div class="recommend">
-        <div class="recommend-content">
+        <scroll class="recommend-content" :data="discList">
+          <div>
             <div v-if="recommends.length" class="slider-wrapper">
                 <slider>
-                    <div v-for="item in recommends" :key="item.linkUrl">
+                    <div v-for="(item,index) in recommends" :key="index">
                         <a :href="item.linkUrl">
                             <img :src="item.picUrl" alt="">
                         </a>
                     </div>
                 </slider>
             </div>
-        </div>
-        <div class="recommend-list">
-            <h1 class="list-title">热门歌单推荐</h1>
-            <ul>
-
-            </ul>
-        </div>
+            <div class="recommend-list">
+              <h1 class="list-title">热门歌单推荐</h1>
+              <ul>
+                <li v-for="(item,index) in discList" :key="index" class="item">
+                  <div class="icon">
+                    <img :src="item.imgurl" alt="imgIcon" width="60" height="60">
+                  </div>
+                  <div class="text">
+                    <h2 class="name" v-html="item.creator.name"></h2>
+                    <p class="desc" v-html="item.dissname"></p>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </scroll>
     </div>
 </template>
 
@@ -24,14 +34,17 @@
 import {getRecommend, getDiscList} from 'api/recommend'
 import {ERR_OK} from 'api/config'
 import Slider from 'base/slider/slider'
+import Scroll from 'base/scroll/scroll'
 export default {
   data () {
     return {
-      recommends: []
+      recommends: [],
+      discList: []
     }
   },
   components: {
-    Slider
+    Slider,
+    Scroll
   },
   created () {
     this._getRecommend()
@@ -50,8 +63,8 @@ export default {
     _getDiscList () {
       getDiscList().then((res) => {
         if (res.code === ERR_OK) {
-          // this.discList = res.data.list
-          console.log(res.data.list)
+          this.discList = res.data.list
+          // console.log(res.data.list)
         }
       })
     }
@@ -82,18 +95,25 @@ export default {
           font-size: $font-size-medium
           color: $color-theme
         .item
+          /* item使用flex布局 */
           display: flex
           box-sizing: border-box
+          /* item内部的元素在交叉轴上中心对齐（即水平居中） */
           align-items: center
           padding: 0 20px 20px 20px
           .icon
+            /* 项目设置flex，存在剩余空间时不放大；当空间不足时其他项目缩小，该项目不缩小；该项目初始大小20px*20px */
             flex: 0 0 60px
             width: 60px
             padding-right: 20px
           .text
+            /* 项目使用flex布局 */
             display: flex
+            /* 主轴方向为colum */
             flex-direction: column
+            /* text内部元素在主轴上中心对齐（即垂直居中） */
             justify-content: center
+            /* 等价于flex:1 1 auto ，即存在剩余空间时放大，且由于icon设置flex-shrink为0，导致text在空间不足时优先缩小，auto自动补充多余空间 */
             flex: 1
             line-height: 20px
             overflow: hidden
